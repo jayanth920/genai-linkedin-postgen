b05dd6d0b02e4e4e1b133a8eba4effdcddaeb74c
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2025-04-18 16:46:51 -0500

    libs added to requirements.txt

--- llm_helper.py
+++ llm_helper.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-llm = ChatGroq(groq_api_key = os.getenv("GROQ_API_KEY"), model_name="llama-3.2-90b-vision-preview")
+llm = ChatGroq(groq_api_key = os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
 
 if __name__ == '__main__':
     response = llm.invoke("What are main ingredients in Pizza")
